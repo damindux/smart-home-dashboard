b3084b7a0918f492c22dac05608484e1cea951cc
Damindu Sandeepa <72655390+damindux@users.noreply.github.com>
2024-01-08 22:40:58 +0530

    Integrate firebase into the project
    Set up firebase connection in a separate javascript file and link it to
    the index.html

---
+++ js/database_connect.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAJeQwT-fGmhKQoinG9xhXfn0HCKN2sRzI",
+    authDomain: "smart-home-2195e.firebaseapp.com",
+    databaseURL: "https://smart-home-2195e-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "smart-home-2195e",
+    storageBucket: "smart-home-2195e.appspot.com",
+    messagingSenderId: "60295512862",
+    appId: "1:60295512862:web:af1afbf87493d03b8dbf0f"
+};
+  
+const app = initializeApp(firebaseConfig);
