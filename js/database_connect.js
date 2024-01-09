2466ae8fa3a0b7c209c3b0615adf6f47f03d031c
Damindu Sandeepa <72655390+damindux@users.noreply.github.com>
2024-01-09 15:21:18 +0530

    Create logic for smart home tools in the dashboard
    Add eventlisteners for the smart home appliances in the
    database_connect.js file.

--- js/database_connect.js
+++ js/database_connect.js
@@ -11,3 +11,69 @@ const firebaseConfig = {
 };
   
 const app = initializeApp(firebaseConfig);
+
+const database = app.database();
+
+const temperatureRef = database.ref('living_room/temperature');
+const humidityRef = database.ref('living_room/humidity');
+const livingRoomLightRef = database.ref('living_room/light');
+const livingRoomFanRef = database.ref('living_room/fan');
+
+const bathroomLightRef = database.ref('bath_room/light');
+
+const bedroomLightRef = database.ref('bed_room/light');
+
+const kitchenLightRef = database.ref('kitchen/light');
+
+const mainDoorRef = database.ref('main_door/lock');
+
+// Temperature and Humidity
+temperatureRef.on('value', (snapshot) => {
+    const temperature = snapshot.val();
+    document.getElementById('temp').textContent = temperature;
+});
+
+humidityRef.on('value', (snapshot) => {
+    const humidity = snapshot.val();
+    document.getElementById('hum').textContent = humidity;
+});
+
+// Living Room
+const livingRoomLightSwitch = document.getElementById('lrl-switch');
+const livingRoomFanSlider = document.getElementById('fan');
+
+livingRoomLightSwitch.addEventListener('change', () => {
+    livingRoomLightRef.transaction(currentValue => !currentValue);
+});
+
+livingRoomFanSlider.addEventListener('change', () => {
+    livingRoomFanRef.set(livingRoomFanSlider.value);
+});
+
+// Bathroom
+const bathroomLightSwitch = document.getElementById('btl-switch');
+
+bathroomLightSwitch.addEventListener('change', () => {
+    bathroomLightRef.transaction(currentValue => !currentValue);
+});
+
+// Bedroom
+const bedroomLightSwitch = document.getElementById('bdl-switch');
+
+bedroomLightSwitch.addEventListener('change', () => {
+    bedroomLightRef.transaction(currentValue => !currentValue);
+});
+
+// Kitchen
+const kitchenLightSwitch = document.getElementById('kl-switch');
+
+kitchenLightSwitch.addEventListener('change', () => {
+    kitchenLightRef.transaction(currentValue => !currentValue);
+});
+
+// Main Door
+const mainDoorSwitch = document.getElementById('d-lock');
+
+mainDoorSwitch.addEventListener('change', () => {
+    mainDoorRef.transaction(currentValue => !currentValue);
+});
